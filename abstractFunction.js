da3cb6d37528e08156354612e99be6ed1a046cc1
Tycko Franklin <TheTyckoMan@gmail.com>
2018-12-02 22:22:26 -0800

    Added an abstract method to convert any callback into a promise.

---
+++ abstractFunction.js
@@ -0,0 +1,23 @@
+
+
+function cTPResolveRejectArguments(callbackBasedFunction, nArguments) {
+    let [a, ...b] = arguments;
+    return new Promise(function (resolve, reject) {
+        a(resolve, reject, ...b);
+    });
+}
+
+function cTPArgumentResolve(callbackBasedFunction, options) {    
+    return new Promise(function (resolve, reject) {
+        try {
+            callbackBasedFunction(options, resolve);
+        } catch (e) {
+            return reject(e);
+        }
+    });
+}
+
+
+cTPArgumentResolve(chrome.tabs.query, { active: true })
+.then((a) => { console.log(a) })
+.catch((a) => { console.log("Error message in catch:  ", a) });
